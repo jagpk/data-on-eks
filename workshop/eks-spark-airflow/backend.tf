62d0478bf3589a9582e2a3865b2d035b08f2e767
Jagdeep Phoolkumar <jphoolku@amazon.com>
2023-09-26 15:35:11 +0800

    workshop

---
+++ workshop/eks-spark-airflow/backend.tf
@@ -0,0 +1,8 @@
+ #this will be injected by the steps in the buildspec file
+#terraform {
+#  backend "s3" {
+#    bucket = "$datapart.$region.terraform-state-nagra"
+#    key    = "$applicationname/terraform.tfstate"
+#    region = "$region"
+#  }
+#}
